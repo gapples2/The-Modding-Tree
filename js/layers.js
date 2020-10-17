ee6af9432b7030196eeea95ff0a21893af910e87
gapples2 <71836730+gapples2@users.noreply.github.com>
2020-10-17 06:41:55 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -4,15 +4,37 @@ addLayer("b", {
         unlocked: true,
         points: new Decimal(0),
     }},
-    color: "brown",
-    resource: "Basic Points", 
-    type: "none",
-    row: 0,
-    layerShown() {return true}, 
-    tooltip() { // Optional, tooltip displays when the layer is locked
-        return ("Basic Points")
+    color: "#FE0102",                       // The color for this layer, which affects many elements
+    resource: "basic points",            // The name of this layer's main prestige resource
+    row: 0,                                 // The row this layer is on (0 is the first row)
+
+    baseResource: "dust",                 // The name of the resource your prestige gain is based on
+    baseAmount() {return player.points},    // A function to return the current value of that resource
+
+    requires: new Decimal(10),            // The amount of the base needed to  gain 1 of the prestige currency.
+                                            // Also the amount required to unlock the layer.
+    
+    type: "normal",                         // Determines the formula used for calculating prestige currency.
+    exponent: 0.5,                          // "normal" prestige gain is (currency^exponent)
+
+    gainMult() {                            // Returns your multiplier to your gain of the prestige resource
+        return new Decimal(1)               // Factor in any bonuses multiplying gain here
     },
-}, 
+    gainExp() {                             // Returns your exponent to your gain of the prestige resource
+        return new Decimal(1)
+    },
+
+    layerShown() {return true},             // Returns a bool for if this layer's node should be visible in the tree.
+    }, 
+    upgrades, {
+        rows: 1,
+        cols: 1,
+        11: {
+            desc: "Blah",
+            
+        },
+        etc
+    }
 )
 
 
