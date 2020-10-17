71f5b68de9e0418a31d161dbabdf533963d6c8cc
gapples2 <71836730+gapples2@users.noreply.github.com>
2020-10-17 09:42:18 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,32 +1,30 @@
-// This layer is mostly minimal but it uses a custom prestige type and a clickable
 addLayer("b", {
     startData() { return {
         unlocked: true,
         points: new Decimal(0),
     }},
-    color: "#FE0102",                       // The color for this layer, which affects many elements
-    resource: "basic points",            // The name of this layer's main prestige resource
-    row: 0,                                 // The row this layer is on (0 is the first row)
+    color: "#FE0102", 
+    resource: "basic points",            
+    row: 0,                                 
 
-    baseResource: "dust",                 // The name of the resource your prestige gain is based on
-    baseAmount() {return player.points},    // A function to return the current value of that resource
+    baseResource: "dust",                 
+    baseAmount() {return player.points},    
 
-    requires: new Decimal(10),            // The amount of the base needed to  gain 1 of the prestige currency.
-                                            // Also the amount required to unlock the layer.
+    requires: new Decimal(10),            
+                                            
     
-    type: "normal",                         // Determines the formula used for calculating prestige currency.
-    exponent: 0.5,                          // "normal" prestige gain is (currency^exponent)
+    type: "normal",                         
+    exponent: 0.5,                          
 
-    gainMult() {                            // Returns your multiplier to your gain of the prestige resource
-        return new Decimal(1)               // Factor in any bonuses multiplying gain here
+    gainMult() {                            
+        return new Decimal(1)               
     },
-    gainExp() {                             // Returns your exponent to your gain of the prestige resource
+    gainExp() {                             
         return new Decimal(1)
     },
 
-    layerShown() {return true},             // Returns a bool for if this layer's node should be visible in the tree.
-    }, 
-    upgrades, {
+    layerShown() {return true},  
+    upgrades: {
         rows: 1,
         cols: 1,
         11: {
@@ -34,9 +32,9 @@ addLayer("b", {
             
         },
         etc
-    }
+    }            
+},
 )
-
 
 addLayer("a", {
         startData() { return {
@@ -70,8 +68,7 @@ addLayer("a", {
                 name: "Even more basic.",
                 done() {return player.b.points.gte(10)},
                 goalTooltip: "Basic*10.", // Shows when achievement is not completed
-                doneTooltip: "Get a farm point.\n\nReward: The dinosaur is now your friend (you can max Farm Points).", // Showed when the achievement is completed
-                onComplete() {console.log("Bork bork bork!")}
+                doneTooltip: "Way too basic."
             },
         },
         midsection: [
