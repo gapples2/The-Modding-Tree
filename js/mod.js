e91e8577faaa061098e72436dae0b866791c7c41
gapples2 <71836730+gapples2@users.noreply.github.com>
2021-04-22 12:50:32 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Tree of Challenges",
+	id: "nadauciduasudcbasuicdbnascu",
+	author: "gapples2",
 	pointsName: "points",
 	discordName: "",
 	discordLink: "",
